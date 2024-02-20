0b100d5ae326dc5b9b434121bb067a54795fb115
zlinzhou <zlinzhou@amazon.com>
2024-02-20 16:04:47 +0000

    Initial skeleton and dummy CRUDL handlers of Ingress Point

---
+++ aws-ses-mailmanageringresspoint/src/main/java/software/amazon/ses/mailmanageringresspoint/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ses.mailmanageringresspoint;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ses-mailmanageringresspoint.json");
+    }
+}
