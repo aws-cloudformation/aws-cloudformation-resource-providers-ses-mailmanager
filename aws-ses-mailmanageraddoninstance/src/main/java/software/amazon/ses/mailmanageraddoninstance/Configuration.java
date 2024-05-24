f944f5b94f706b5ec8ad146d45d236efb382c852
zlinzhou <zlinzhou@amazon.com>
2024-05-24 13:54:24 +0100

    Introduce Add-on Instance & Subscription

---
+++ aws-ses-mailmanageraddoninstance/src/main/java/software/amazon/ses/mailmanageraddoninstance/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ses.mailmanageraddoninstance;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ses-mailmanageraddoninstance.json");
+    }
+}
---
+++ aws-ses-mailmanageraddonsubscription/src/main/java/software/amazon/ses/mailmanageraddonsubscription/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ses.mailmanageraddonsubscription;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ses-mailmanageraddonsubscription.json");
+    }
+}
